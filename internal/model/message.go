e26ded8bd723263316b9fa0f88517b64d6b854b3
djc <daijunchao1127@qq.com>
2026-04-19 00:11:28 +0800

    [feature][model] added message.go room.go user.go

---
+++ internal/model/message.go
@@ -0,0 +1,41 @@
+package model
+
+import (
+	"encoding/json"
+	"time"
+)
+
+const (
+	TypeDanmaku = 101
+	TypeStatus  = 102
+	ActionLike  = 103
+)
+
+type wsPacket struct {
+	Type   int             `json:"type"`
+	RoomID string          `json:"room_id,omitempty"`
+	Data   json.RawMessage `json:"data"`
+}
+
+type DanmakuMessage struct {
+	ID       uint64    `gorm:"primaryKey;autoIncrement" json:"id"`
+	RoomID   string    `gorm:"type:varchar(50);not null;index:idx_room_time" json:"room_id"`
+	UserID   uint64    `gorm:"not null;index" json:"user_id"`
+	Content  string    `gorm:"type:varchar(500);not null" json:"content"`
+	SendTime time.Time `gorm:"type:datetime(3);not null;index:idx_room_time" json:"send_time"`
+}
+
+func (DanmakuMessage) TableName() string {
+	return "danmaku_message"
+}
+
+// for TypeStatus
+type StatsData struct {
+	Online uint64 `json:"online"`
+	Likes  uint64 `json:"likes"`
+}
+
+// for ActionLike
+type Like struct {
+	Count uint64 `json:"count"`
+}
